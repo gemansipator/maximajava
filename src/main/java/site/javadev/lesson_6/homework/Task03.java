2980fb7d72c672befb7d2cf4d03b249969ad9d8c
gemansipator <gemansipator@gmail.com>
2024-09-11 00:38:15 +0300

    Task03 1. Создать массив на 10 строк.         2. Ввести с клавиатуры 8 строк и сохранить их в массив.         3. Вывести содержимое всего массива (10 элементов) на экран в обратном порядке.         Каждый элемент - с новой строки.

---
+++ src/main/java/site/javadev/lesson_6/homework/Task03.java
@@ -0,0 +1,22 @@
+package site.javadev.lesson_6.homework;
+
+import java.util.Scanner;
+
+/**1. Создать массив на 10 строк.
+ 2. Ввести с клавиатуры 8 строк и сохранить их в массив.
+ 3. Вывести содержимое всего массива (10 элементов) на экран в обратном порядке.
+ Каждый элемент - с новой строки.*/
+public class Task03 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        String [] str = new String[10];
+        for (int i = 0; i < 8; i++) {
+            str[i] = scanner.next();
+        }
+        for (int i = 9; i < str.length ; i--) {
+            if (i >= 0) {
+                System.out.println(str[i]);
+            }
+        }
+    }
+}
