c817c539458c52f21d304e33635cc6f4348cc979
gemansipator <gemansipator@gmail.com>
2024-09-11 04:17:06 +0300

    Task05 1. Считать 6 строк и заполнить ими массив strings.         2. Удалить повторяющиеся строки из массива strings,         заменив их на null (null должны быть не строками "null")

--- src/main/java/site/javadev/lesson_06/homework/Task05.java
+++ src/main/java/site/javadev/lesson_06/homework/Task05.java
@@ -1,28 +1,36 @@
 package site.javadev.lesson_06.homework;
 
+import java.util.Arrays;
+import java.util.Objects;
 import java.util.Scanner;
 
-/**1. Считать 6 строк и заполнить ими массив strings.
- 2. Удалить повторяющиеся строки из массива strings,
- заменив их на null (null должны быть не строками "null")*/
+/** 1. Считать 6 строк и заполнить ими массив strings.
+ * 2. Удалить повторяющиеся строки из массива strings,
+ *    заменив их на null (null должны быть не строками "null")
+ */
 public class Task05 {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        String [] str  = new String[6];
-        for (int i = 0; i < 6; i++) {
-            str[i] = scanner.next();
+        String[] str = new String[6];
 
+        // Считываем строки и заполняем массив
+        for (int i = 0; i < str.length; i++) {
+            str[i] = scanner.nextLine();
         }
-        for (int i = 0; i < 6; i++) {
-            for (int j = i + 1; j < 6; j++) {
-                if (str[i].equals(str[j])) {
-                    str[j] = "null";
+
+        // Поиск и замена повторяющихся строк на null
+        for (int i = 0; i < str.length; i++) {
+            if (str[i] == null) {
+                continue; // Пропускаем, если элемент уже null
+            }
+            for (int j = i + 1; j < str.length; j++) {
+                if (Objects.equals(str[i], str[j])) {
+                    str[j] = null; // Если строки совпадают, заменяем на null
                 }
             }
         }
-        for (int i = 0; i < 6; i++) {
-            System.out.println(str[i]);
-        }
-    }
 
+        // Выводим массив
+        System.out.println(Arrays.toString(str));
+    }
 }
