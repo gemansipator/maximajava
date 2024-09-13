761ae07c1f201c63a62c6b1129d918aa3148b667
Nikolay <gemansipator@gmail.com>
2024-09-13 16:38:56 +0300

    Написать простой консольный калькулятор на Java.  Метод int getInt() - должен считывать с консоли целое число и возвращать его  Метод char getOperation() - должен считывать с консоли какое-то значение и возвращать символ с операцией  (+, -, * или /)  Метод int calc(int num1, int num2, char operation) - должен выполнять над числами num1 и num2  арифметическую операцию, заданную operation.  Метод main() - должен считывать 2 числа (с помощью getInt()), считать операцию (с помощью getOperation(),  передать все методу calc() и вывести на экран результат.

---
+++ src/main/java/site/javadev/lesson_07/Task05.java
@@ -0,0 +1,65 @@
+package site.javadev.lesson_07;
+
+import java.util.Scanner;
+
+/**Написать простой консольный калькулятор на Java.
+ Метод int getInt() - должен считывать с консоли целое число и возвращать его
+ Метод char getOperation() - должен считывать с консоли какое-то значение и возвращать символ с операцией
+ (+, -, * или /)
+ Метод int calc(int num1, int num2, char operation) - должен выполнять над числами num1 и num2
+ арифметическую операцию, заданную operation.
+ Метод main() - должен считывать 2 числа (с помощью getInt()), считать операцию (с помощью getOperation(),
+ передать все методу calc() и вывести на экран результат.*/
+public class Task05 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter number: ");
+
+        int num1 = getInt(sc);
+        int num2 = getInt(sc);
+        char operation = getOperation(sc);
+        System.out.println(calc(num1, num2, operation));
+
+        sc.close();
+
+    }
+
+    private static char getOperation(Scanner sc) {
+        return sc.next().charAt(0);
+    }
+
+    private static int getInt(Scanner sc) {
+        return sc.nextInt();
+    }
+    private static double calc(int num1, int num2, char operation) {
+        // Проверка, что num2 < num1 только для деления
+        if (operation == '/' && num2 > num1) {
+            System.out.println("Ошибка: num2 должно быть меньше num1 для деления, чтобы результат был больше 0");
+            return -1; // Указываем на ошибку
+        }
+        double result;
+        switch (operation) {
+            case '+':
+                result = num1 + num2;
+                break;
+            case '-':
+                result =  num1 - num2;
+                break;
+            case '*':
+                result = num1 * num2;
+                break;
+            case '/':
+                if (num2 == 0) {
+                    System.out.println("Делить на ноль нельзя");
+                    return -1; //для указания ошибки
+                }
+                result = (double) num1 / num2; // Приведение к double для корректного деления
+                break;
+            default:
+                System.out.println("Ошибка: неизвестная операция");
+                return -1;  //для указания ошибки
+        }
+        return result;
+    }
+
+}
