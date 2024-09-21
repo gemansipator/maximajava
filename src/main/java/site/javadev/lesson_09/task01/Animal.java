64847c3182b6761a80382df536dfd2f133c31820
gemansipator <gemansipator@gmail.com>
2024-09-21 15:21:01 +0300

    Создать класс Animal и расширяющие его классы Dog, Cat, Horse.  Класс Animal содержит переменные food, location и методы makeNoise, eat, sleep. Метод makeNoise, например, может  выводить на консоль "Такое-то животное спит".  Dog, Cat, Horse переопределяют методы makeNoise, eat.  Добавьте переменные в классы Dog, Cat, Horse, характеризующие только этих животных.  Создайте класс Ветеринар, в котором определите метод void treatAnimal(Animal animal). Пусть этот метод распечатывает  food и location пришедшего на прием животного.  В методе main создайте массив типа Animal, в который запишите животных всех имеющихся у вас типов. В цикле отправляйте  их на прием к ветеринару.

---
+++ src/main/java/site/javadev/lesson_09/task01/Animal.java
@@ -0,0 +1,12 @@
+package site.javadev.lesson_09.task01;
+
+public abstract class Animal {
+    String food;
+    String location;
+
+    abstract void makeNoise();
+
+    abstract void eat();
+
+    abstract void sleep();
+}
