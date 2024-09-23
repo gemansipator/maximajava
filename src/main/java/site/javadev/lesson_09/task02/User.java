ebc5b047c22e5c88551d3ab38002b65dc6edb611
gemansipator <gemansipator@gmail.com>
2024-09-23 14:57:22 +0300

    Task02 private public class User {     private String login;     private String password;     private Basket basket;
        public User(String login, String password, Basket basket) {
            this.login = login;
            this.password = password;
            this.basket = basket;
        }
    }

--- src/main/java/site/javadev/lesson_09/task02/User.java
+++ src/main/java/site/javadev/lesson_09/task02/User.java
@@ -3,7 +3,7 @@ package site.javadev.lesson_09.task02;
 public class User {
     private String login;
     private String password;
-    Basket basket;
+    private Basket basket;
 
     public User(String login, String password, Basket basket) {
         this.login = login;
