4c393215b94e3a344e931fe22e336e81993953cd
gemansipator <gemansipator@gmail.com>
2024-09-23 14:59:11 +0300

    Task02 private

--- src/main/java/site/javadev/lesson_09/task02/Category.java
+++ src/main/java/site/javadev/lesson_09/task02/Category.java
@@ -1,8 +1,8 @@
 package site.javadev.lesson_09.task02;
 
 public class Category {
-    String name; // имя категории
-    Product[] products; // массив товаров
+    private String name; // имя категории
+    private Product[] products; // массив товаров
     public Category(String name, Product[] products) {
         this.name = name;
         this.products = products;
--- src/main/java/site/javadev/lesson_09/task02/Product.java
+++ src/main/java/site/javadev/lesson_09/task02/Product.java
@@ -1,9 +1,9 @@
 package site.javadev.lesson_09.task02;
 
 public class Product {
-    String name; // имя товара
-    double price; // цена товара
-    double rating; // рейтинг товара
+    private String name; // имя товара
+    private double price; // цена товара
+    private double rating; // рейтинг товара
 
     public Product(String name, double price, double rating) {
         this.name = name;
