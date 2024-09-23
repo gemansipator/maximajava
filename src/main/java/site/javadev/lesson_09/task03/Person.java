615ffb6cb06d5c93b8dabdf75627c73cc119a2bf
gemansipator <gemansipator@gmail.com>
2024-09-23 15:00:02 +0300

    Task03 private

--- src/main/java/site/javadev/lesson_09/task03/Person.java
+++ src/main/java/site/javadev/lesson_09/task03/Person.java
@@ -2,8 +2,8 @@ package site.javadev.lesson_09.task03;
 
 public class Person {
 
-    String fullName; // полное имя человека
-    int age; // возраст человека
+    private String fullName; // полное имя человека
+    private int age; // возраст человека
 
     void move(){  // человек движется
         System.out.println(fullName + " is moving.");
