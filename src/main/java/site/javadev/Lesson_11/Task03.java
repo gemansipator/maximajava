3a84de1ba7e1e5ebe29db4b3b2a74b15eaca2d84
gemansipator <gemansipator@gmail.com>
2024-09-29 16:45:35 +0300

    Task03 убрал лишний indexOf и добавил переменные для хранения индекса

--- src/main/java/site/javadev/Lesson_11/Task03.java
+++ src/main/java/site/javadev/Lesson_11/Task03.java
@@ -9,11 +9,13 @@ import java.util.Scanner;
  3. Узнай, какая строка в списке встретится раньше: самая короткая или самая длинная.
  Если таких строк несколько, то должны быть учтены самые первые из них.
  4. Выведи на экран строку из п.3. Должна быть выведена одна строка.*/
+
 public class Task03 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         List<String> list = new ArrayList<>();
 
+        // Запрашиваем 10 строк у пользователя
         for (int i = 0; i < 10; i++) {
             list.add(sc.nextLine());
         }
@@ -21,25 +23,30 @@ public class Task03 {
         // Инициализируем переменные для хранения самой длинной и самой короткой строки
         String minValueString = list.get(0);
         String maxValueString = list.get(0);
+        int minIndex = 0, maxIndex = 0;
 
+        // Ищем самую короткую и самую длинную строку
         for (int i = 0; i < 10; i++) {
-            if (list.get(i).length() < minValueString.length()) {
-                minValueString = list.get(i);
+            String currentString = list.get(i);
+            // Проверяем, является ли текущая строка короче
+            if (currentString.length() < minValueString.length()) {
+                minValueString = currentString;
+                minIndex = i;  // Обновляем индекс
             }
-            if (list.get(i).length() > maxValueString.length()) {
-                maxValueString = list.get(i);
+            // Проверяем, является ли текущая строка длиннее
+            if (currentString.length() > maxValueString.length()) {
+                maxValueString = currentString;
+                maxIndex = i;  // Обновляем индекс
             }
-
         }
 
         // Сравниваем индексы самой короткой и самой длинной строки
-        if (list.indexOf(minValueString) < list.indexOf(maxValueString)) {
+        if (minIndex < maxIndex) {
             System.out.println(minValueString); // Выводим самую короткую строку, если она была раньше
         } else {
             System.out.println(maxValueString); // Выводим самую длинную строку, если она была раньше
         }
 
-
-        sc.close();
+        sc.close(); // Закрываем сканер
     }
 }
