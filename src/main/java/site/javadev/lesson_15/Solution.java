8852d496097f33d8877dd3791b7fe17a0b910fd0
gemansipator <gemansipator@gmail.com>
2024-10-20 08:35:19 +0300

    метод removeAllNumbersGreaterThan10 теперь void. Теперь не возвращаем ссылу set но напрямую меняем значение set.removeIf(number -> number > 10); И числа добавляются в начале циклом for согласно предложениям

--- src/main/java/site/javadev/lesson_15/Solution.java
+++ src/main/java/site/javadev/lesson_15/Solution.java
@@ -10,33 +10,30 @@ package site.javadev.lesson_15;
  •    Класс Solution должен содержать только три метода.
  •    Метод createSet() должен создавать и возвращать множество Set состоящих из 20 различных чисел.
  •    Метод removeAllNumbersGreaterThan10() должен удалять из множества все числа больше 10.*/
-import java.util.Collections;
+
 import java.util.HashSet;
-import java.util.Iterator;
 import java.util.Set;
 
 public class Solution {
     public static Set<Integer> createSet() {
         // напишите тут ваш код
 
-        Set<Integer> set = new HashSet<Integer>();
-        Collections.addAll(set, 1, 2, 3, 40, 5, 6, 7, 8, 9, 10, 11, 12, 13, 111, 15, 16, 17, 18, 19, 20);
+        Set<Integer> set = new HashSet<>();
+        for (int i = 1; i <= 20; i++) {
+            set.add(i);
+        }
         return set;
     }
 
-
-    public static Set<Integer> removeAllNumbersGreaterThan10(Set<Integer> sset) {
-        Iterator<Integer> iterator = sset.iterator();
-        while (iterator.hasNext()) {
-            int number = iterator.next();
-            if (number > 10) {
-                iterator.remove();
-            }
-        }
-
-        return sset;
+    public static void removeAllNumbersGreaterThan10(Set<Integer> set) {
+        set.removeIf(number -> number > 10);
     }
     public static void main(String[] args) {
+        // Создание множества
+        Set<Integer> numbers = createSet();
+
+        // Удаление чисел больше 10
+        removeAllNumbersGreaterThan10(numbers);
 
     }
 
