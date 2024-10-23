322fb608221b3c7f24d0078a31d64831faa3b95a
gemansipator <gemansipator@gmail.com>
2024-10-23 17:17:26 +0300

    JUnit тест

---
+++ src/test/java/site/javadev/lesson_16/UserTest.java
@@ -0,0 +1,29 @@
+package site.javadev.lesson_16;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class UserTest {
+
+    @Test
+    public void testUserCreation() {
+        // Создаем нового пользователя
+        User user = new User("Иван", 18);
+
+        // Проверяем, что пользователь создан с правильными данными
+        Assertions.assertEquals("Иван", user.getName());
+        Assertions.assertEquals(18, user.getAge());
+    }
+
+    @Test
+    public void testUserAgeUpdate() {
+        // Создаем нового пользователя
+        User user = new User("Мария", 25);
+
+        // Обновляем возраст
+        user.setAge(26);
+
+        // Проверяем, что возраст обновился
+        Assertions.assertEquals(26, user.getAge());
+    }
+}
